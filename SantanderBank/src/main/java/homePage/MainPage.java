4117fa212a847bcb921d1e2cd54d9ae10bd846a0
MdManiruzzaman <159manir@gmail.com>
2019-01-11 18:13:59 -0500

    Revised Commit

--- SantanderBank/src/main/java/homePage/MainPage.java
+++ SantanderBank/src/main/java/homePage/MainPage.java
@@ -14,6 +14,36 @@ public class MainPage extends CommonAPI {
         }.getClass().getEnclosingMethod().getName()));
         SantanderLogo.click();
     }
+    @FindBy(xpath = "//input[@type ='text' and @id = '_P001_Header_WAR_P001_Headerportlet_search']")
+    public static WebElement SearchBox;
 
+    public void checkSearchBox() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        SearchBox.click();
+    }
+    @FindBy(xpath = "//a[text()='Personal']")
+    public static WebElement Personal;
 
+    public void checkPersonal() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        Personal.click();
+    }
+    @FindBy(xpath = "//a[text()='Business' and @class='txtOpts ']")
+    public static WebElement Business;
+
+    public void checkBusiness() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        Business.click();
+    }
+    @FindBy(xpath = "//a[text()='Commercial']")
+    public static WebElement Commercial;
+
+    public void checkCommercial() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        Commercial.click();
+    }
 }
--- SantanderBank/src/test/java/testHomePage/TestMainPage.java
+++ SantanderBank/src/test/java/testHomePage/TestMainPage.java
@@ -12,7 +12,7 @@ public class TestMainPage extends MainPage {
     @BeforeMethod
     public void initialize() {
         TestLogger.log(convertToString(new Object(){}.getClass().getSimpleName()) + " : " + convertToString(new Object() {
-        }.getClass().getEnclosingMethod().getName())); //implemented by Nasir
+        }.getClass().getEnclosingMethod().getName()));
         mainPage = PageFactory.initElements(driver, MainPage.class);
     }
     @Test
@@ -20,5 +20,28 @@ public class TestMainPage extends MainPage {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         mainPage.checkSantanderLogo();
     }
-
+    @Test
+    public void testSearchBox() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkSearchBox();
+    }
+    @Test
+    public void testPersonal() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkPersonal();
+    }
+    @Test
+    public void testBusiness() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkBusiness();
+    }
+    @Test
+    public void testCommercial() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        mainPage.checkCommercial();
+    }
 }
