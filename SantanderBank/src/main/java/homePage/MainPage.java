7facb02753d96c03bd5cdf204c5b4ca22324dfa1
MdManiruzzaman <159manir@gmail.com>
2019-01-11 20:27:48 -0500

    Test case Updated

--- SantanderBank/src/main/java/homePage/MainPage.java
+++ SantanderBank/src/main/java/homePage/MainPage.java
@@ -46,4 +46,133 @@ public class MainPage extends CommonAPI {
         }.getClass().getEnclosingMethod().getName()));
         Commercial.click();
     }
+    @FindBy(xpath = "//a[text()='About' and @class='txtOpts ']")
+    public static WebElement viewAbout;
+
+    public void checkviewAbout() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewAbout.click();
+    }
+
+    @FindBy(xpath = "//h1[@class='titpage alignC']")
+    public static WebElement viewSantanderCommercialBanking;
+
+    public void checkviewSantanderCommercialBanking() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewSantanderCommercialBanking.click();
+    }
+
+    @FindBy(xpath = "//span[text()='Banking']")
+    public static WebElement viewBanking;
+
+    public void checkviewBanking() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewBanking.click();
+    }
+
+    @FindBy(xpath = " //span[text()='Borrowing']")
+    public static WebElement viewBorrowing;
+
+    public void checkviewBorrowing() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewBorrowing.click();
+    }
+
+    @FindBy(xpath = " //span[text()='Investments and Protection']")
+    public static WebElement viewInvestmentsAndProtection;
+
+    public void checkviewInvestmentsAndProtection() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewInvestmentsAndProtection.click();
+    }
+
+    @FindBy(xpath = " //span[text()='Credit Cards']")
+    public static WebElement viewCreditCards;
+
+    public void checkviewCreditCards() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewCreditCards.click();
+    }
+
+    @FindBy(xpath = "//span[text()='Prosper and Thrive']")
+    public static WebElement viewProsperAndThrive;
+
+    public void checkviewProsperAndThrive() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewProsperAndThrive.click();
+    }
+
+    @FindBy(xpath = "//a[text() = 'ATM/Branch Locator']")
+    public static WebElement viewATMBranchLocator;
+
+    public void checkviewATMBranchLocator() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        viewATMBranchLocator.click();
+    }
+
+    @FindBy(xpath = "//a[@class='button01 trk-btn']")
+    public static WebElement findALocationNearYou;
+
+    public void checkFindALocationNearYou() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        findALocationNearYou.click();
+    }
+
+    @FindBy(xpath = "//h4[text()='Contact Us']")
+    public static WebElement contactUs;
+
+    public void checkContactUs() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        contactUs.click();
+    }
+    @FindBy(xpath = "//h4[text()='Privacy Policy']")
+    public static WebElement privacyPolicy;
+
+    public void checkPrivacyPolicy() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        privacyPolicy.click();
+    }
+    @FindBy(xpath = "//h4[text()='Terms of Use']")
+    public static WebElement termsOfUse;
+
+    public void checkTermsOfUse() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        termsOfUse.click();
+    }
+    @FindBy(xpath = "//h4[text()='Home']")
+    public static WebElement home;
+
+    public void checkHome() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){
+        }.getClass().getEnclosingMethod().getName()));
+        home.click();
+    }
+    @FindBy(xpath = "//p[contains(text(),'Useful Links and Documents')]")
+    public static WebElement UsefulLinksAndDocuments;
+
+    public void checkUsefulLinksAndDocuments() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){
+        }.getClass().getEnclosingMethod().getName()));
+        UsefulLinksAndDocuments.click();
+    }
+    @FindBy(xpath = "//a[@href='#Checking']")
+    public static WebElement Checking;
+
+    public void checkChecking() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){
+        }.getClass().getEnclosingMethod().getName()));
+        Checking.click();
+    }
 }
